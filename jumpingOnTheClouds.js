24a7ca724b0c8f725fa1a3884ac86dee93b8f65c
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-04-22 04:08:18 +0100

    Jumping on clouds first attempt

--- jumpingOnTheClouds.js
+++ jumpingOnTheClouds.js
@@ -1,6 +1,19 @@
 function jumpingOnClouds(c) {
+    let numOfJumps = 0;
 
+    for (let i = 0; i < c.length; i++) {
+        // if next cloud = 0 jump
+        // if cloud = 0 jump
+        let cloud = c[i]
+        let nextCloud = c[i + 1]
+        if (cloud === 0) {
+            if (nextCloud === 0) {
+                numOfJumps++
+            }
+        }
+    }
 
+    return numOfJumps;
 }
 
-jumpingOnClouds([0, 1, 0, 0, 0, 1, 0])
+console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0]));
