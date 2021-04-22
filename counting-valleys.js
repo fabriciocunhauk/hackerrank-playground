954df30d3686da0648baabbe129adf8660b232cd
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-04-22 02:41:10 +0100

    Counting valleys solved

--- counting-valleys.js
+++ counting-valleys.js
@@ -1,41 +1,60 @@
 function countingValleys(steps, path) {
-    let obj = {};
     let upAndDownPointer = 0;
-    let downCount = 0;
     let valley = 0;
 
     for (var i = 0; i < path.length; i++) {
-        console.log(upAndDownPointer)
+        const keyChar = path[i];
 
-
-        obj['char'] = path[i];
-        console.log(obj);
-
-
-
-        if (obj.char === "U") {
-            upAndDownPointer++
-        }
-        if (obj.char === "D") {
+        if (keyChar === "D") {
             upAndDownPointer--
-        }
 
-        if (upAndDownPointer < 0) {
-            downCount = 1;
-        }
-        if (upAndDownPointer > 0) {
-            downCount = 0;
-        }
-        if (downCount === 1 && upAndDownPointer === 0) {
-            valley++;
+        } else {
+            if (upAndDownPointer === -1) {
+                valley++
+            }
+            upAndDownPointer++
         }
     }
 
     return valley;
 }
-console.log(countingValleys(12, ["D", "D", "U", "U", "D", "D", "U", "D", "U", "U", "U", "D", "D", "D", "U", "U", "D", "U"]));
-
-
+console.log(countingValleys(12, ["D", "U", "D", "U", "D", "U", "D", "U", "D", "U", "D", "U", "D", "U", "D", "U"]));
 
 // Test 1 (8, ["U", "D", "D", "D", "U", "D", "U", "U"])
 // Test 2 (12, ["D","D","U","U","D","D","U","D","U","U","U","D"])
+
+//Naive solution
+
+// function countingValleys(steps, path) {
+//     let obj = {};
+//     let upAndDownPointer = 0;
+//     let downCount = 0;
+//     let valley = 0;
+
+//     for (var i = 0; i < path.length; i++) {
+//         console.log(upAndDownPointer)
+
+//         obj['char'] = path[i];
+//         console.log(obj);
+
+//         if (obj.char === "U") {
+//             upAndDownPointer++
+//         }
+//         if (obj.char === "D") {
+//             upAndDownPointer--
+//         }
+
+//         if (upAndDownPointer < 0) {
+//             downCount = 1;
+//         }
+//         if (upAndDownPointer > 0) {
+//             downCount = 0;
+//         }
+//         if (downCount === 1 && upAndDownPointer === 0) {
+//             valley++;
+//         }
+//     }
+
+//     return valley;
+// }
+// console.log(countingValleys(12, ["D", "D", "U", "U", "D", "D", "U", "D", "U", "U", "U", "D", "D", "D", "U", "U", "D", "U"]));
