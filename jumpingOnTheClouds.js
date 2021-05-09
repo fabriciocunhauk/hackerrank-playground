e8817e54e7bbf08e6cf00e2cc32207e785696b93
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-05-09 02:09:23 +0100

    Jumping clouds Solved

--- jumpingOnTheClouds.js
+++ jumpingOnTheClouds.js
@@ -2,18 +2,18 @@ function jumpingOnClouds(c) {
     let numOfJumps = 0;
 
     for (let i = 0; i < c.length; i++) {
-        // if next cloud = 0 jump
-        // if cloud = 0 jump
         let currentCloud = c[i]
         let nextCloud = c[i + 1]
-        let next2Cloud = c[i + 2]
-        if (currentCloud === nextCloud && currentCloud === next2Cloud) {
-            currentCloud = next2Cloud;
+        let afterNextCloud = c[i + 2]
+
+        if (nextCloud === currentCloud && afterNextCloud === currentCloud) {
+            c.shift();
+        }
+        if (nextCloud === 0) {
             numOfJumps++;
         }
     }
-
     return numOfJumps;
 }
 
-console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0]));
+console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]));
