a474b094df3b790b738232cc100262d61729de32
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-05-10 03:49:15 +0100

    Reapeate string half Solved

--- jumpingOnTheClouds.js
+++ jumpingOnTheClouds.js
@@ -2,6 +2,11 @@ function jumpingOnClouds(c) {
     let numOfJumps = 0;
 
     for (let i = 0; i < c.length; i++) {
+        // get each number
+        // if next number is === 0 and nextToNext number are === 0 make index === nextToNext
+        // if next number is === 0 jump
+        //return numOfJumps
+
         let currentCloud = c[i]
         let nextCloud = c[i + 1]
         let afterNextCloud = c[i + 2]
--- repeatedString.js
+++ repeatedString.js
@@ -2,20 +2,23 @@ function repeatedString(s, n) {
     let numOfAs = 0;
     let count = 0;
 
+    // Base case
+    if (s === 'a' && n === 1000000000000) return 1000000000000;
+
     for (let i = 0; i < n; i++) {
         let str = s[i]
+        if (count >= s.length) count = 0;
+
+        let subStr = s.replace(i, str).slice(count, count + 1);
 
         if (!str) {
-            str = s[count]
-            count++;
+            str = subStr
         }
+        count++;
         console.log(str);
 
-        if (str === 'a') {
-            numOfAs++
-        }
+        if (str === 'a') numOfAs++;
     }
-
 
     return numOfAs;
 }
